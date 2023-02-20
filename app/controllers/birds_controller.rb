2867b78f34f6b33f356143185a7d3aff2a317c2b
wendy <mizukilee@gmail.com>
2023-02-20 18:03:55 -0500

    commit tree 2438434af45418b4ef7415c51958aef902eb55dd

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,10 @@ class BirdsController < ApplicationController
     end
   end
 
+  # post /birds 
+  def create 
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created 
+  end
+
 end
